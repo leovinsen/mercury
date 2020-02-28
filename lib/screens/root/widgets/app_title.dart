e1162168588d5d9d5a8a1e8748ca24aab1623b16
Leonardo Vinsen <leonardovinsen@gmail.com>
2020-02-29 01:47:46 +0700

    Added app icon to splash screen

--- lib/screens/root/widgets/app_title.dart
+++ lib/screens/root/widgets/app_title.dart
@@ -6,10 +6,17 @@ class AppTitle extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
+        Image.asset(
+          'assets/ic_launcher.png',
+          height: 250,
+          width: 250,
+        ),
         Text(
           'Mercury',
-          style:
-              Theme.of(context).textTheme.display2.copyWith(color: Colors.black, fontWeight: FontWeight.bold),
+          style: Theme.of(context)
+              .textTheme
+              .display2
+              .copyWith(color: Colors.black, fontWeight: FontWeight.bold),
         ),
         SizedBox(
           height: 4.0,
@@ -19,7 +26,9 @@ class AppTitle extends StatelessWidget {
           width: 200.0,
           color: Colors.black,
         ),
-        SizedBox(height: 4.0,),
+        SizedBox(
+          height: 4.0,
+        ),
         Container(
           height: 3.0,
           width: 190.0,
